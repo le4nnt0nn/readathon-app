fc64d0c0fc57fb7c0a83b1d997329307750b5b39
Antonio Cortés <71550295+le4nnt0nn@users.noreply.github.com>
2026-05-26 23:20:20 +0200

    refactor: change structure

--- read-app/frontend/web/src/app/layout/shell/shell.component.ts
+++ read-app/frontend/web/src/app/layout/shell/shell.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { AuthService } from '../../core/auth.service';
 import { NgIf } from '@angular/common';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-shell',
@@ -14,31 +25,36 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       transition(':enter', [
         style({
           opacity: 0,
-          transform: 'translateY(-10px) scale(0.95)'
+          transform: 'translateY(-10px) scale(0.95)',
         }),
-        animate('180ms cubic-bezier(.16,.8,.24,1)',
+        animate(
+          '180ms cubic-bezier(.16,.8,.24,1)',
           style({
             opacity: 1,
-            transform: 'translateY(0) scale(1)'
-          })
-        )
+            transform: 'translateY(0) scale(1)',
+          }),
+        ),
       ]),
       transition(':leave', [
-        animate('140ms ease-in',
+        animate(
+          '140ms ease-in',
           style({
             opacity: 0,
-            transform: 'translateY(-8px) scale(0.96)'
-          })
-        )
-      ])
-    ])
-  ]
+            transform: 'translateY(-8px) scale(0.96)',
+          }),
+        ),
+      ]),
+    ]),
+  ],
 })
 export class ShellComponent {
   menuOpen = false;
   securityOpen = false;
   usersOpen = false;
-  constructor(public auth: AuthService, private router: Router) {}
+  constructor(
+    public auth: AuthService,
+    private router: Router,
+  ) {}
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
@@ -56,7 +72,7 @@ export class ShellComponent {
     this.usersOpen = !this.usersOpen;
     this.securityOpen = false;
   }
-  
+
   toggleSecurity() {
     this.securityOpen = !this.securityOpen;
     this.usersOpen = false;
